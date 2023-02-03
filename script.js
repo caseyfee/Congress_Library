3a65298cd78f3936432b5ef5bbc6e351fc41f707
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-02-02 21:05:01 -0800

    Sudo codes in js

---
+++ script.js
@@ -0,0 +1,14 @@
+// reference id for user inputs
+//      For keywords
+//      For formats
+
+// reference id for buttons
+//      search
+//      goback
+//      readmore
+// 
+
+// Actions buttons
+// Click on the "click me" button
+
+    // create a function
